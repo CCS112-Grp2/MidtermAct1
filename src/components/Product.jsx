4c283696299da52a7ee90a5eba99f62e5419eec8
matinong-username <132667404+matinong-username@users.noreply.github.com>
2024-04-06 09:07:27 +0800

    products

---
+++ src/components/Product.jsx
@@ -0,0 +1,15 @@
+Product.jsx:
+
+import React from 'react';
+
+const Product = ({ name, description, price, addToCart }) => {
+  return (
+    <div className="product">
+      <h3>{name}</h3>
+      <p>{description}</p>
+      <p className="price">${price}</p>
+    </div>
+  );
+};
+
+export default Product;
