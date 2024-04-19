2350d2e06ff165b45c1104e9f7cfb3a46c4d267c
JpaulTorres <115199483+JpaulV2@users.noreply.github.com>
2024-04-20 07:03:27 +0800

    Update ViewCartButton.jsx

--- src/components/Cart/ViewCartButton.jsx
+++ src/components/Cart/ViewCartButton.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
 
+// ViewCartButton component to display a button to view the cart
 const ViewCartButton = ({ onClick }) => {
   return (
+    // Button to view the cart
     <Button variant="primary" onClick={onClick}>
       MY CART
     </Button>
