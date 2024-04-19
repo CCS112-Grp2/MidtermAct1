361b8991359976e8a56bf524d27762797e66317e
matinong-username <132667404+Gutierrez94@users.noreply.github.com>
2024-04-20 07:24:28 +0800

    homepage logo

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 const Home = () => (
   <div className="container text-center text-white">
     <br></br><br></br><br></br><br></br>
+    <img src="https://img.freepik.com/premium-vector/eco-friendly-ride-monochrome-vectorized-symbol-advanced-ev-concept-black-emblematic-sketch_706143-47025.jpg" alt="" className="me-2" style={{ height: '300px', width: '300px' }} />
     <h1>Welcome to GGGHI Car Dealer</h1>
     <p className="lead">BUY YOUR DREAM CAR TODAY!</p>
     <Link to="/products" className="btn btn-primary">PROCEED TO SHOPPING</Link>
