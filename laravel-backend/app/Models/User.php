a1d0e69153edded24ae3e912554d6c0a83bb0c41
Hilario <hilariorencegabriel917@gmail.com>
2024-05-04 09:40:02 +0800

    Updated User.php

--- laravel-backend/app/Models/User.php
+++ laravel-backend/app/Models/User.php
@@ -14,6 +14,8 @@ class User extends Authenticatable
     /**
      * The attributes that are mass assignable.
      *
+     *
+     *
      * @var array<int, string>
      */
     protected $fillable = [
